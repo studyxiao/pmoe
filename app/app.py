82301a5bf07a459b2732a904efdeb7125bb068b1
studyxiao <studyxiao@163.com>
2023-06-24 11:23:19 +0800

    fix: 移除测试任务

--- app/app.py
+++ app/app.py
@@ -11,10 +11,4 @@ app.config.from_object(config)
 
 @app.get("/")
 def index() -> str:
-    from task.example import send_email, send_sms, send_sms2
-
-    for _ in range(10):
-        send_email.apply_async(args=("hello",))
-        send_sms.apply_async(args=("hello",))
-        send_sms2.apply_async(args=("hello",))
     return "Hello World!"

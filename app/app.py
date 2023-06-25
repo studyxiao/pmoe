21eb0ed5e8ab3201f6b51ca951b69966f900c8e5
studyxiao <studyxiao@163.com>
2023-06-25 18:55:56 +0800

    refactor: 添加flask插件

--- app/app.py
+++ app/app.py
@@ -1,14 +1,20 @@
 from flask import Flask
 
 from app.core.log import Logger
+from app.core.model import db
 from config import config
 
 app = Flask(__name__)
 
-Logger(app)
+# 配置项
 app.config.from_object(config)
 
+# 插件
+Logger(app)
+db.init_app(app)
 
+
+# 路由
 @app.get("/")
 def index() -> str:
     return "Hello World!"
